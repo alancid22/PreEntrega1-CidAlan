7aa801c6867ee0f083616de33528c5cc73179641
Alan Cid <alancid12@gmail.com>
2023-07-19 19:09:27 -0300

    correción preentrega

--- index.js
+++ index.js
@@ -1,112 +1,35 @@
-// let continuar = true;
-
 let nombreUsuario = prompt("Define un nombre de usuario.");
 
 alert("Bienvenido " + nombreUsuario + " al juego de preguntas y respuestas. Vamos a comenzar ! ");
 
-    let opcion = parseInt(prompt("¿Cuál es la capital de Argentina? :\n 1. Santiago\n 2. Buenos Aires\n 3. Lima"));
-
-    switch(opcion) {
-        case 1: // if opcion === 1 
-            alert("Respuesta incorrecta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta correcta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta incorrecta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
+const preguntas = [
+    { pregunta: "¿Cuál es la capital de Argentina? Escriba el numero de la opcion correcta :\n 1) Santiago\n 2) Buenos Aires\n 3) Lima" , respuesta: "2" },
+    { pregunta: "¿En que año Sudafrica organizo el mundial de Futbol? Escriba el numero de la opcion correcta :\n 1) 2002\n 2) 2014\n 3) 2010", respuesta: "3" },
+    { pregunta: "¿Quien fue elegido presidente de Argentina en el año 2003? Escriba el numero de la opcion correcta :\n 1) Nestor Kirchner\n 2) Eduardo Duhalde\n 3) Carlos Menem", respuesta: "1" },
+    { pregunta: "¿Qué artista pintó La noche estrellada? Escriba el numero de la opcion correcta :\n 1) Van Gogh\n 2) Picasso\n 3) Da Vinci", respuesta: "1" },
+    { pregunta: "¿Cuántos elementos hay en la Tabla Periódica? Escriba el numero de la opcion correcta :\n 1) 118 elementos\n 2) 114 elementos\n 3) 116 elementos", respuesta: "1" }
+  ];
+  
+  // Función para realizar la prueba
+  function realizarPrueba() {
+    let respuestasCorrectas = 0;
+  
+    // Iterar sobre las preguntas
+    for (let i = 0; i < preguntas.length; i++) {
+      const respuestaUsuario = prompt(preguntas[i].pregunta);
+      
+      // Comprobar si la respuesta es correcta
+      if (respuestaUsuario.toLowerCase() === preguntas[i].respuesta.toLowerCase()) {
+        respuestasCorrectas++;
+      }
     }
-
-    let opcion2 = parseInt(prompt("¿En que año Sudafrica organizo el mundial de Futbol? :\n 1. 2002\n 2. 2014\n 3. 2010"));
-
-    switch(opcion2) {
-        case 1: // if opcion === 1 
-            alert("Respuesta incorrecta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta incorrecta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta correcta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
-    }
-
-    let opcion3 = parseInt(prompt("¿Quien fue elegido presidente de Argentina en el año 2003? :\n 1. Nestor Kirchner\n 2. Eduardo Duhalde\n 3. Carlos Menem"));
-
-    switch(opcion3) {
-        case 1: // if opcion === 1 
-            alert("Respuesta correcta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta incorrecta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta incorrecta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
-    }
-
-    let opcion4 = parseInt(prompt("¿En qué continente se encuentra Ecuador? :\n 1. Asia\n 2. America\n 3. Africa"));
-
-    switch(opcion4) {
-        case 1: // if opcion === 1 
-            alert("Respuesta incorrecta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta correcta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta incorrecta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
-    }
-
-    let opcion5 = parseInt(prompt("¿Qué artista pintó La noche estrellada? :\n 1. Van Gogh\n 2. Picasso\n 3. Da Vinci"));
-
-    switch(opcion5) {
-        case 1: // if opcion === 1 
-            alert("Respuesta correcta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta incorrecta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta incorrecta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
-    }
-
-    let opcion6 = parseInt(prompt("¿Cuántos elementos hay en la Tabla Periódica? :\n 1. 118 elementos\n 2. 114 elementos\n 3. 116 elementos"));
-
-    switch(opcion6) {
-        case 1: // if opcion === 1 
-            alert("Respuesta correcta");
-            break;
-        case 2: // else if opcion === 2 
-            alert("Respuesta incorrecta");
-            break;
-        case 3: // else if opcion === 3 
-            alert("Respuesta incorrecta");
-            break;
-        default: // else
-            alert("Respuesta incorrecta");
-            break;
-    }
-
-alert("Felicidades " + nombreUsuario + " has terminado el cuestionario de cultura general.");
-
-
-
+  
+    // Calcular porcentaje de respuestas correctas
+    const porcentajeCorrectas = (respuestasCorrectas / preguntas.length) * 100;
+  
+    // Mostrar resultado
+    alert( `` + nombreUsuario + ` has respondido correctamente ${respuestasCorrectas} de ${preguntas.length} preguntas.\nPorcentaje de respuestas correctas: ${porcentajeCorrectas}%`);
+  }
+  
+  // Llamar a la función para iniciar la prueba
+  realizarPrueba();
