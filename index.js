51a0a2525fb4a417b0bcf8198380416acedcfd71
Alan Cid <alancid12@gmail.com>
2023-07-19 19:32:47 -0300

    correción preentrega

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const preguntas = [
     { pregunta: "¿Quien fue elegido presidente de Argentina en el año 2003? Escriba el numero de la opcion correcta :\n 1) Nestor Kirchner\n 2) Eduardo Duhalde\n 3) Carlos Menem", respuesta: "1" },
     { pregunta: "¿Qué artista pintó La noche estrellada? Escriba el numero de la opcion correcta :\n 1) Van Gogh\n 2) Picasso\n 3) Da Vinci", respuesta: "1" },
     { pregunta: "¿Cuántos elementos hay en la Tabla Periódica? Escriba el numero de la opcion correcta :\n 1) 118 elementos\n 2) 114 elementos\n 3) 116 elementos", respuesta: "1" }
-  ];
+];
   
-  // Función para realizar la prueba
-  function realizarPrueba() {
+// Función para realizar la prueba
+function realizarPrueba() {
     let respuestasCorrectas = 0;
   
     // Iterar sobre las preguntas
@@ -22,14 +22,14 @@ const preguntas = [
       if (respuestaUsuario.toLowerCase() === preguntas[i].respuesta.toLowerCase()) {
         respuestasCorrectas++;
       }
-    }
+}
   
-    // Calcular porcentaje de respuestas correctas
-    const porcentajeCorrectas = (respuestasCorrectas / preguntas.length) * 100;
+// Calcular porcentaje de respuestas correctas
+const porcentajeCorrectas = (respuestasCorrectas / preguntas.length) * 100;
   
-    // Mostrar resultado
-    alert( `` + nombreUsuario + ` has respondido correctamente ${respuestasCorrectas} de ${preguntas.length} preguntas.\nPorcentaje de respuestas correctas: ${porcentajeCorrectas}%`);
-  }
+// Mostrar resultado
+alert( `` + nombreUsuario + ` has respondido correctamente ${respuestasCorrectas} de ${preguntas.length} preguntas.\nPorcentaje de respuestas correctas: ${porcentajeCorrectas}%`);
+}
   
-  // Llamar a la función para iniciar la prueba
-  realizarPrueba();
+// Llamar a la función para iniciar la prueba
+realizarPrueba();
